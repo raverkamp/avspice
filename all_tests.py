8a4f4541f9bce2cf1593b6e40901595897e49e48
Roland Averkamp <roland.averkamp.0@gmail.com>
2021-01-06 19:42:45 +0100

    added unit test

--- all_tests.py
+++ all_tests.py
@@ -34,6 +34,7 @@ class Test1(unittest.TestCase):
          self.assertAlmostEqual(r[r1][0], 1)
          self.assertAlmostEqual(r[r1][1], 1/20)
 
+    # diode and resistor in serial
     def test_diode_simple(self):
          net = Network()
          d1 = net.addComp("d1", DIODE)
@@ -46,8 +47,30 @@ class Test1(unittest.TestCase):
          analy = Analysis(net)
          (v,r,d) = analy.analyze()
          pp.pprint((v,r,d))
+         # check current is the same
          self.assertAlmostEqual(r[r1][1], d[d1][1])
 
+
+   # diode - diode - resistor
+    def test_diode_simple2(self):
+         net = Network()
+         d1 = net.addComp("d1", DIODE)
+         d2 = net.addComp("d2", DIODE)
+         r1 = net.addR("r1", 500)
+         v1 = net.addV("v1", 5)
+         connect(v1.n, net.ground)
+         connect(v1.p, d1.p)
+         connect(d1.n, d2.p)
+         connect(d2.n, r1.p)
+         connect(r1.n, v1.n)
+         analy = Analysis(net)
+         (v,r,d) = analy.analyze()
+         pp.pprint((v,r,d))
+         # check current is the same
+         self.assertAlmostEqual(r[r1][1], d[d1][1])
+         self.assertAlmostEqual(r[r1][1], d[d2][1])
+         # check voltage diff over both diodes is equal
+         self.assertAlmostEqual(d[d1][0], d[d2][0])
 
 
 if __name__ == '__main__':
