f0abc3e354b998bf05ce76ac6ba54d3fc5e3dceb
Roland Averkamp <roland.averkamp.0@gmail.com>
2022-01-22 18:50:33 +0100

    blinker with analysis.transient method

--- blink1.py
+++ blink1.py
@@ -9,7 +9,7 @@ from example_circuits import create_blinker
 
     
 def blinker(args):
-    net = create_blinker(transistor_gain=100, r_ca=0.01)
+    net = create_blinker(transistor_gain=100, r_ca=100e-6)
     ana = Analysis(net)
 
     base_vca = args.vca   #0.397 #-5 # -8.25
@@ -47,7 +47,7 @@ def blinker(args):
     cond = []
     s = 1e-4 #0.001/1
     x = 0
-    v_ca = base_vca
+    v_ca = res.get_voltage("ca.p") - res.get_voltage("ca.n")
     switched = False
     ca_cu = 0
     printstep = 0.1
@@ -139,65 +139,12 @@ def blinker(args):
     plt.show()
     input()
 
-def catest(args):
-    net = Network()
-    vc = net.addV("vc",  9) 
-    r1 = net.addR("r1", 1e3)
-    c = net.addCapa("capa", 1e-6)
-
-    connect(vc.p,r1.p)
-    connect(r1.n, c.p)
-    connect(c.n,vc.n)
-    connect(vc.n, net.ground)
-
-    ana = Analysis(net)
-    base_vca = 0
-    v_ca = base_vca
-    ca_cu = 0
-    sol = None
-    res = ana.analyze(maxit=50, start_solution_vec=sol, capa_voltages={"capa": base_vca},
-                  variables={"vc": 9})
-    if isinstance(res, str):
-        raise Exception("can not find inital solution for time sweep")
-    sol = res.solution_vec
-
-    x = 0
-    s = 0.0001
-    ca_v = []
-    ca_i = []
-    xs =[]
-    while x < 0.01:
-        print("///", x)
-        ok = False
-        maxit = 50
-        capa = net.get_object("capa").capa
-        vca_new = v_ca + s*ca_cu/capa
-        v_ca = vca_new
-        res = ana.analyze(maxit=maxit, start_solution_vec=sol, capa_voltages={"capa": v_ca })
-        if isinstance(res,str):
-            break
-        ca_cu = res.get_current("capa.p")
-        xs.append(x)
-        x = x + s
-        ca_v.append(v_ca)
-        ca_i.append(ca_cu)
-
-    fig, ((a1,a2, a3)) = plt.subplots(1,3)
-    
-    a1.plot(xs,ca_v, label="v(ca)")
-    a1.legend()
-    a2.plot(xs, ca_i, label="i(ca)")
-    a2.legend()
-    
-    plt.ion()
-    plt.show()
-    input()
 
 def blinker_static(args):
     net = create_blinker()
     ana = Analysis(net)
 
-    base_vca = 0.0   #0.397 #-5 # -8.25
+    base_vca = -0.4   #0.397 #-5 # -8.25
 
 
     sol = None
@@ -207,21 +154,14 @@ def blinker_static(args):
                           "t1.C": 4,
                           "t2.C": 0.1,
                           "t1.B": 0.1})
-    print(res)
-    vn = res.get_voltage("ca.n")
-    vp = res.get_voltage("ca.p")
-    print("voltage ca ={0}".format(vp-vn))
-    
-    for s in ["t1.C", "t1.B", "t2.C", "t2.B"]:
-            print("{0} V = {1}   I={2}".format(s, res.get_voltage(s), res.get_current(s)))
-
+    res.display()
 
 
 def try_random_start(args):
     import random
     net = create_blinker(r_ca=1e-1)
     ana = Analysis(net)
-    base_vca = 0.0
+    base_vca = 0
     res = ana.analyze(maxit=50, start_solution_vec=None, capa_voltages={"ca": base_vca},
                       variables={"vc": 9},
                       start_voltages= {
@@ -235,6 +175,71 @@ def try_random_start(args):
         for s in ["t1.C", "t1.B", "t2.C", "t2.B"]:
             print("{0} V = {1}   I={2}".format(s, res.get_voltage(s), res.get_current(s)))
 
+
+def pivot(res):
+    time = []
+    volts = {}
+    currs = {}
+    for (t,v,c) in res:
+        time.append(t)
+        for k in v:
+            if not k in volts:
+                volts[k] = []
+            volts[k].append(v[k])
+        for k in c:
+            if not k in currs:
+                currs[k] = []
+            currs[k].append(c[k])
+    for k in volts:
+        volts[k] = np.array(volts[k])
+    for k in currs:
+        currs[k] = np.array(currs[k])
+
+                            
+    return (np.array(time),volts,currs)
+                            
+
+def blinker3(args):
+    net = create_blinker(transistor_gain=100, r_ca=100e-6)
+    ana = Analysis(net)
+
+    base_vca = args.vca
+
+    res = ana.transient(3.56, 0.001, capa_voltages={"ca": base_vca},
+                        variables={"vc": 9},
+                        start_voltages= {
+                          "t1.C": args.t1c,
+                           "t1.B": args.t1b
+                        })
+
+    (time,volts,currs) = pivot(res)
+    fig, ((a1,a2, a3), (a4, a5, a6), (a7,a8,a9)) = plt.subplots(3,3)
+    a1.plot(time,currs["t1.C"],label="curr t1.C")
+    a1.legend()
+    a2.plot(time,currs["t2.C"], label="curr t2.C")
+    a2.legend()
+    a3.plot(time,currs["ca.p"], label="curr ca.p")
+    a3.legend()
+
+    a4.plot(time,volts["t1.B"],label="volt t1.B, ca.n")
+    a4.plot(time,volts["t1.E"],label="volt t1.E")
+    a4.plot(time,volts["ca.p"],label="volt ca.p")
+    a4.plot(time,volts["t1.C"],label="volt t1.C")
+    a4.plot(time,volts["t2.C"],label="volt t2.C")
+    a4.legend()
+    a5.plot(time,currs["t1.B"],label="curr t1.B")
+    a5.legend()
+    a6.plot(time,currs["t1.E"],label="curr t1.E")
+    a6.legend()
+    a7.plot(time, volts["ca.p"] - volts["ca.n"], label="voltage capa")
+    a7.legend()
+    a8.plot(time,currs["t2.B"],label="curr t2.B")
+    a8.legend()
+    
+    plt.show()
+                            
+    
+            
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
@@ -244,10 +249,7 @@ def main():
     parser_b.add_argument('-t1c', type=float, default=0)
     parser_b.add_argument('-t1b', type=float, default=0)
     parser_b.add_argument('-vca', type=float, default=0)
-
-    parser_c = subparsers.add_parser('c')
-    parser_c.set_defaults(func=catest)
-
+    
     parser_s = subparsers.add_parser('s')
     parser_s.set_defaults(func=blinker_static)
 
@@ -255,7 +257,12 @@ def main():
     parser_sol.set_defaults(func=try_random_start)
     parser_sol.add_argument('-t1c', type=float, default=0)
     parser_sol.add_argument('-t1b', type=float, default=0)
-    
+
+    parser_b3 = subparsers.add_parser('b3')
+    parser_b3.set_defaults(func=blinker3)
+    parser_b3.add_argument('-t1c', type=float, default=0)
+    parser_b3.add_argument('-t1b', type=float, default=0)
+    parser_b3.add_argument('-vca', type=float, default=0)
     
     args = parser.parse_args()
     args.func(args)
