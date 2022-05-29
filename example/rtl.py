22706371cae309077c98dcad7f1317c2a9ecaf91
Roland Averkamp <roland.averkamp.0@gmail.com>
2022-05-29 14:02:58 +0200

    nor2 gate

--- example/rtl.py
+++ example/rtl.py
@@ -14,6 +14,17 @@ def create_not_gate(*,ri, rc):
     sc.addR("ri", ri, "i", "b")
     return sc
 
+def create_nor2_gate(*, ri, rc):
+    sc =  SubCircuit(("vc", "0", "i1","i2", "o"))
+
+    sc.addR("rc", rc, "vc","o")
+
+    sc.addR("ri1", ri, "i1", "b1")
+    sc.add_component("T1", tnpn, ("b1","o","0"))
+
+    sc.addR("ri2", ri, "i2", "b2")
+    sc.add_component("T2", tnpn, ("b2","o","0"))
+    return sc
 
 def not1(args):
     net = Circuit()
@@ -39,7 +50,7 @@ def not1(args):
     res.display()
 
 
-def not2(args):
+def not_not(args):
     net = Circuit()
 
     vi = Variable("vi")
@@ -66,19 +77,55 @@ def not2(args):
     print(f"----   not2 vi={args.vi} -----")
     res.display()
 
+def nor2(args):
+    net = Circuit()
+
+    vi1 = Variable("vi1")
+    vi2 = Variable("vi2")
+
+    ri = 1e4
+    rc = 1e3
+    ro = ri/3
+
+    ng = create_nor2_gate(ri=ri, rc=rc)
+
+    net.addV("V", 5, "v","0")
+
+    net.addV("vi1", vi1, "i1", "0")
+    net.addV("vi2", vi2, "i2", "0")
+
+    net.add_subcircuit("n1", ng, ("v","0","i1","i2", "o"))
+
+    net.addR("ro", ro, "o", "0")
+
+
+    ana = Analysis(net)
+
+    res= ana.analyze(variables={"vi1": args.vi1, "vi2": args.vi2})
+    print(f"----   nor2 vi={args.vi1} {args.vi2} -----")
+    res.display()
+
+
+
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(required=True)
 
-    p_not1 = subparsers.add_parser('not1')
-    p_not1.set_defaults(func=not1)
-    p_not1.add_argument("vi", type=float)
+    p_not = subparsers.add_parser('not')
+    p_not.set_defaults(func=not1)
+    p_not.add_argument("vi", type=float)
 
-    p_not2d = subparsers.add_parser('not2')
-    p_not2d.set_defaults(func=not2)
-    p_not2d.add_argument("vi", type=float)
+    p_not_not = subparsers.add_parser('not_not')
+    p_not_not.set_defaults(func=not_not)
+    p_not_not.add_argument("vi", type=float)
+
+    p_nor2 = subparsers.add_parser('nor2')
+    p_nor2.set_defaults(func=nor2)
+    p_nor2.add_argument("vi1", type=float)
+    p_nor2.add_argument("vi2", type=float)
 
 
     args = parser.parse_args()
     args.func(args)
+
 main()
