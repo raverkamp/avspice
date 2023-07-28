d7a50a404fd26165a8f3d947f1042fb0b46f27db
Roland Averkamp <roland.averkamp@accenture.com>
2023-07-28 12:35:18 +0200

    imprióved long tailed pair example

--- example/long_tailed_pair.py
+++ example/long_tailed_pair.py
@@ -7,12 +7,8 @@ from avspice import Diode, NPNTransistor, Circuit, SubCircuit, Variable, Analysi
 from avspice.util import drange
 
 
-def mk(args):
-    beta = 100
-
+def create_circuit(rc, re, beta):
     dv = 5
-    rc = 1e3
-    re = 10e3
 
     tt = NPNTransistor("tt", 1e-12, 25e-3, beta, 10)
 
@@ -35,8 +31,17 @@ def mk(args):
     net.addV("VP", vp, "ip", "0")
     net.addV("VN", vn, "in", "0")
 
+    return net
+
+
+def cmd_analysis(args):
+    net = create_circuit(rc=args.rc, re=args.re, beta=args.beta)
+
+    signal_p = args.v + args.dv / 2
+    signal_n = args.v - args.dv / 2
+
     ana = Analysis(net)
-    res = ana.analyze(variables={"vp": args.vp, "vn": args.vn})
+    res = ana.analyze(variables={"vp": signal_p, "vn": signal_n}, maxit=args.maxit)
     if isinstance(res, str):
         print(res)
     else:
@@ -51,14 +56,56 @@ def mk(args):
         )
 
 
+def cmd_plot(args):
+    beta = args.beta
+    rc = args.rc
+    re = args.re
+    net = create_circuit(rc=rc, re=re, beta=beta)
+
+    x = []
+    y = []
+    z = []
+    dv = 0.001
+    ana = Analysis(net)
+    for v in drange(1, 4, 0.01):
+        x.append(v)
+
+        signal_p = v + dv / 2
+        signal_n = v - dv / 2
+        res = ana.analyze(variables={"vp": signal_p, "vn": signal_n}, maxit=args.maxit)
+        if isinstance(res, str):
+            y.append(None)
+            z.append(None)
+        else:
+            y.append((res.get_voltage("t1.C") - res.get_voltage("t2.C")) / dv)
+            z.append(res.get_voltage("t1.C"))
+
+    fig, (ax1, ax2) = plt.subplots(2)
+    ax1.plot(x, y)
+    ax2.plot(x, z)
+    ax1.set_title(f"beta={beta}, rc={rc}, re={re}")
+    plt.show()
+
+
 def main():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(required=True)
 
     parser_comp = subparsers.add_parser("comp")
-    parser_comp.set_defaults(func=mk)
-    parser_comp.add_argument("vp", type=float, default=0)
-    parser_comp.add_argument("vn", type=float, default=0)
+    parser_comp.set_defaults(func=cmd_analysis)
+    parser_comp.add_argument("v", type=float, default=0)
+    parser_comp.add_argument("dv", type=float, default=0)
+    parser_comp.add_argument("-rc", type=float, default=1000)
+    parser_comp.add_argument("-re", type=float, default=1e3)
+    parser_comp.add_argument("-beta", type=float, default=100)
+    parser_comp.add_argument("-maxit", type=int, default=20)
+
+    parser_plot = subparsers.add_parser("plot")
+    parser_plot.set_defaults(func=cmd_plot)
+    parser_plot.add_argument("-rc", type=float, default=1000)
+    parser_plot.add_argument("-re", type=float, default=1e3)
+    parser_plot.add_argument("-beta", type=float, default=100)
+    parser_plot.add_argument("-maxit", type=int, default=20)
 
     args = parser.parse_args()
     args.func(args)
